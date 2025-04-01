fd8e24b042b457779f0fb5d0a94b3f7aa768e5e8
Vicente Zepeda Mas <vzepedam@redhat.com>
2025-04-01 10:05:00 +0200

    Change rate for baseline measures
    Signed-off-by: Vicente Zepeda Mas <vzepedam@redhat.com>

--- scripts/run_ocm_api_load.sh
+++ scripts/run_ocm_api_load.sh
@@ -6,11 +6,21 @@
 # get-machine-pools 5/s 5\n
 # get-cluster-logs 5/s 5\n
 export tests="
-list-clusters 20/s 5 \n
-get-provision-shards 20/s 5\n
-get-versions 20/s 5\n
-get-cloud-providers 20/s 5\n
-get-addons 20/s 5"
+list-clusters 15/s 5\n
+get-provision-shards 15/s 5\n
+get-versions 15/s 5\n
+get-cloud-providers 15/s 5\n
+get-addons 15/s 5\n
+list-clusters 10/s 5\n
+get-provision-shards 10/s 5\n
+get-versions 10/s 5\n
+get-cloud-providers 10/s 5\n
+get-addons 10/s 5\n
+list-clusters 5/s 5\n
+get-provision-shards 5/s 5\n
+get-versions 5/s 5\n
+get-cloud-providers 5/s 5\n
+get-addons 5/s 5"
 
 create_aws_key(){
     # Delete aws keys if more than 1 key exists
